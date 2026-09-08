ab0ff8362affbca3f89401a5a4b4e16f72246cee
xTian <zxczxckiat@gmail.com>
2026-09-09 01:28:51 +0800

    Update for getting value validation

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ function multiply(x, y){
 }
 
 function divide(x, y){
-    return x / y;
+    return (x / y).toFixed(2);
 }
 
 function operate(x, y, operation){
     switch(operation){
         case '+': 
-            add(x, y);
+            (add(x, y));
             break;
         case '-': 
             subtract(x, y);
@@ -26,9 +26,24 @@ function operate(x, y, operation){
             multiply(x, y);
             break;
         case '/':
-            divide(x, y);
+            console.log(divide(x, y));
             break;
         default:
             alert("Wrong format!");
-    }   
+    }       
 }
+
+const buttonContainer = document.querySelector(".calc-buttons");
+const input = document.querySelector("input").value;
+const buttons = buttonContainer.querySelectorAll("button");
+
+function getValue(input){
+    const regex = /^[0-9+\-*/().\s]+$/;
+    return regex.test(input);
+}
+// function valueButton(btns){
+//     let valueBtn = "98+76-54*32/10";
+
+
+// }
+console.log(operate(2, 5, '/'));
